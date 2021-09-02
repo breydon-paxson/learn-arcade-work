fb1573ff79ca69a4ce21e145fb39e6199e891011
Breydon Paxson <breydon.paxson@my.simpson.edu>
2021-09-02 15:22:32 -0500

    Work on lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,5 @@
+print("""My name is Breydon Paxson and I am a Sophomore at Simpson College! I am on the
+shooting team here! Coach Martin told us \"Keep your grades us to participate!\"""")
+print("""I like to work with this quote in my mind,
+Go\nHard\nor Go\nHome""")
+print("I am glad he is my coach")
