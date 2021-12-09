fd3dca9840d3b2e007d6ea9bd8170bd270ce07b0
Breydon Paxson <breydon.paxson@my.simpson.edu>
2021-12-09 16:29:43 -0600

    finished everything but background

--- Lab 12 - Final Lab/part_12.py
+++ Lab 12 - Final Lab/part_12.py
@@ -18,10 +18,61 @@ SCALE = .3
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-BULLET_SPEED = 5
+BULLET_SPEED = 4
 MAX_PLAYER_BULLETS = 100
 
 ANGLE_SPEED = 5
+
+
+class MenuView(arcade.View):
+    """ Class that manages the 'menu' view. """
+
+    def on_show(self):
+        """ Called when switching to this view"""
+        arcade.set_background_color(arcade.color.WHITE)
+
+    def on_draw(self):
+        """ Draw the menu """
+        arcade.start_render()
+        arcade.draw_text("Menu Screen - click to advance", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
+                         arcade.color.BLACK, font_size=30, anchor_x="center")
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        """ Use a mouse press to advance to the 'game' view. """
+        my_game = MyGame()
+        my_game.setup()
+        self.window.show_view(my_game)
+
+
+class GameOverView(arcade.View):
+    """ Class to manage the game over view """
+    def __init__(self):
+        super().__init__()
+        self.background = arcade.load_texture("stars.png")
+
+    def on_show(self):
+        """ Called when switching to this view"""
+        arcade.background = arcade.load_texture("stars.png")
+
+    def on_draw(self):
+        """ Draw the game over view """
+        arcade.start_render()
+        arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+
+        gameover = f"GAME OVER"
+        arcade.draw_text(gameover, 300, 300, arcade.color.RED, 25)
+
+    def on_key_press(self, key, _modifiers):
+        """ If user hits escape, go back to the main menu view """
+        if key == arcade.key.ESCAPE:
+            menu_view = MenuView()
+            self.window.show_view(menu_view)
+
+
+class TurningSprite(arcade.Sprite):
+    def update(self):
+        super().update()
+        self.angle = math.degrees(math.atan2(self.change_y, self.change_x)) - 90
 
 
 class Ship(arcade.Sprite):
@@ -71,13 +122,13 @@ class Big(arcade.Sprite):
             self.change_y *= -1
 
 
-class MyGame(arcade.Window):
+class MyGame(arcade.View):
     """ Our custom Window Class"""
 
     def __init__(self):
         """ Initializer """
         # Call the parent class initializer
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 12")
+        super().__init__()
 
         self.ship_list = None
         self.big_meteor_list = None
@@ -92,8 +143,6 @@ class MyGame(arcade.Window):
         self.ship_sprite = None
         self.score = 0
         self.lives = 3
-
-        self.set_mouse_visible(False)
 
         self.background = None
 
@@ -155,26 +204,28 @@ class MyGame(arcade.Window):
 
         output = f"Score: {self.score}"
         arcade.draw_text(output, 10, 20, arcade.color.WHITE, 28)
-        if (len(self.big_meteor_list) == 0 and len(self.small_meteor_list)) or len(self.ship_life_list) == 0:
-            gameover = f"GAME OVER"
-            arcade.draw_text(gameover, 300, 300, arcade.color.RED, 25)
+        if len(self.big_meteor_list) == 0 and len(self.small_meteor_list) == 0 or len(self.ship_life_list) == 0:
+            game_over_view = GameOverView()
+            self.window.show_view(game_over_view)
 
     def on_mouse_press(self, x, y, button, modifiers):
-        if len(self.bullet_list) < MAX_PLAYER_BULLETS:
-            arcade.play_sound(self.gun_sound)
-            bullet = arcade.Sprite("laserRed01.png", SPRITE_SCALING_LASER)
-            # Bullet speed
-            bullet.change_y = \
-                math.cos(math.radians(self.ship_sprite.angle)) * BULLET_SPEED
-            bullet.change_x = \
-                -math.sin(math.radians(self.ship_sprite.angle)) * BULLET_SPEED
+        if (len(self.big_meteor_list) > 0 or len(self.small_meteor_list) > 0) and len(self.ship_life_list) > 0:
+            if len(self.bullet_list) < MAX_PLAYER_BULLETS:
+                arcade.play_sound(self.gun_sound)
+                bullet = TurningSprite("laserRed01.png", SPRITE_SCALING_LASER)
 
-            # bullet position
-            bullet.center_x = self.ship_sprite.center_x
-            bullet.center_y = self.ship_sprite.center_y
+                bullet.guid = "Bullet"
 
-            # add the bullet to list
-            self.bullet_list.append(bullet)
+                # Bullet speed
+                bullet.change_y = math.cos(math.radians(self.ship_sprite.angle)) * BULLET_SPEED
+                bullet.change_x = -math.sin(math.radians(self.ship_sprite.angle)) * BULLET_SPEED
+
+                # bullet position
+                bullet.center_x = self.ship_sprite.center_x
+                bullet.center_y = self.ship_sprite.center_y
+
+                # add the bullet to list
+                self.bullet_list.append(bullet)
 
     def on_key_press(self, key, modifiers):
         # turn left
@@ -189,7 +240,7 @@ class MyGame(arcade.Window):
             self.ship_sprite.change_angle = 0
 
     def on_mouse_motion(self, x, y, dx, dy):
-        if len(self.big_meteor_list or self.small_meteor_list) > 0:
+        if (len(self.big_meteor_list) > 0 or len(self.small_meteor_list) > 0) and len(self.ship_life_list) > 0:
             self.ship_sprite.center_x = x
             self.ship_sprite.center_y = y
 
@@ -213,35 +264,47 @@ class MyGame(arcade.Window):
                 self.score += 5
 
     def update(self, delta_time):
+
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 28)
+        if len(self.big_meteor_list) == 0 and len(self.small_meteor_list) == 0 or len(self.ship_life_list) == 0:
+            gameover = f"GAME OVER"
+            arcade.draw_text(gameover, 300, 300, arcade.color.RED, 25)
+            return
+
         self.ship_sprite.angle += self.ship_sprite.change_angle
 
-        if len(self.big_meteor_list or self.small_meteor_list) > 0:
+        if (len(self.big_meteor_list)) > 0 or (len(self.small_meteor_list)) > 0 and (len(self.ship_life_list)) > 0:
             self.big_meteor_list.update()
             self.small_meteor_list.update()
+            self.ship_life_list.update()
 
         big_meteor_hit_list = arcade.check_for_collision_with_list(self.ship_sprite, self.big_meteor_list)
         for big in big_meteor_hit_list:
             big.remove_from_sprite_lists()
             self.score -= 1
             arcade.play_sound(self.big_hit_sound)
-            self.ship_life_list.pop().remove_from_sprite_lists()
-            self.lives -= 1
+            if len(self.ship_life_list) > 0:
+                self.ship_life_list[0].remove_from_sprite_lists()
+                self.lives -= 1
 
         small_meteor_hit_list = arcade.check_for_collision_with_list(self.ship_sprite, self.small_meteor_list)
         for small in small_meteor_hit_list:
             small.remove_from_sprite_lists()
             self.score -= 5
             arcade.play_sound(self.small_hit_sound)
-            self.ship_life_list.pop().remove_from_sprite_lists()
-            self.lives -= 1
+            if len(self.ship_life_list) > 0:
+                self.ship_life_list[0].remove_from_sprite_lists()
+                self.lives -= 1
 
         self.process_ship_bullets()
 
 
 def main():
     """ Main method """
-    window = MyGame()
-    window.setup()
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 12")
+    menu_view = MenuView()
+    window.show_view(menu_view)
     arcade.run()
 
 
